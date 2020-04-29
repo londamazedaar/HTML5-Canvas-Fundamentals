effe6aa06a194978f7f2d879db0e140d5897045e
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-30 04:42:57 +0530

    drawing circle using arc()

--- Drawing_with_canvas.js
+++ Drawing_with_canvas.js
@@ -26,21 +26,29 @@ window.onload = function () {
 
     // =====> Drawing a Bar Graph using transforms(translate)
 
-        const scores = [100,88,92,75,63,33];
-        scores.reverse();
-        let width = 20;
-        let x = 0;
-        let y = 0;
+        // const scores = [100,88,92,75,63,33];
+        // scores.reverse();
+        // let width = 20;
+        // let x = 0;
+        // let y = 0;
         
-        ctx.translate(200,200);
-        ctx.rotate(Math.PI);
-        ctx.fillStyle = 'green';
-        scores.forEach(ele => {
-            ctx.fillRect(x, y, width, this.parseInt(ele));
-            x = x  + width + 5;
-        });
-        ctx.rotate(Math.PI);
-        ctx.translate(-200,-200);
-        ctx.fillRect(0,0,10,10);
+        // ctx.translate(200,200);
+        // ctx.rotate(Math.PI);
+        // ctx.fillStyle = 'green';
+        // scores.forEach(ele => {
+        //     ctx.fillRect(x, y, width, this.parseInt(ele));
+        //     x = x  + width + 5;
+        // });
+        // ctx.rotate(Math.PI);
+        // ctx.translate(-200,-200);
+        // ctx.fillRect(0,0,10,10);
+
+    // =====> Draw circle using arc()
+
+        // ctx.fillStyle = 'red';
+        // ctx.arc(100,100,50,Math.PI / 2,3 * this.Math.PI/2,false);
+        // ctx.strokeStyle = 'blue';
+        // ctx.stroke();
+        // ctx.fill();
 
 }
