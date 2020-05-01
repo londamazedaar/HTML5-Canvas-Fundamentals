ceb4e89e3355b0b07fb7090be61414f4e614ef9b
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-01 21:45:51 +0530

    Animation using SetInterval and RequestAnimationFrame

--- Manipulating_Pixels.js
+++ Manipulating_Pixels.js
@@ -1,6 +1,18 @@
+let radius = 50;
+let posY = 70;
+let posX = radius;
+let canvas, ctx, canvasWidth, canvasHeight;
+let dir = 1;
+let speed = 8;
+
+
 window.onload = function () {
-    const canvas = this.document.getElementById('myCanvas');
-    const ctx = canvas.getContext('2d');
+
+    canvas = this.document.getElementById('myCanvas');
+    canvasWidth = canvas.width;
+    canvasHeight = canvas.height;
+    this.console.log(canvasWidth, canvasHeight);
+    ctx = canvas.getContext('2d');
 
     // =====> Rendering Linear Gradients
 
@@ -29,16 +41,55 @@ window.onload = function () {
 
     // =====> Using Transform
 
-        ctx.font = '30pt Arial';
-        ctx.save();
-        ctx.translate(200,350);
-        ctx.rotate(-0.5*Math.PI);
-        ctx.scale(2,2);
+        // ctx.font = '30pt Arial';
+        // ctx.save();
+        // ctx.translate(200,350);
+        // ctx.rotate(-0.5*Math.PI);
+        // ctx.scale(2,2);
 
-        let rtext = 'Rotated';
-        ctx.fillText(rtext,0,0);
-        ctx.restore();
+        // let rtext = 'Rotated';
+        // ctx.fillText(rtext,0,0);
+        // ctx.restore();
 
-        ctx.fillRect(250,100,200,100);
+        // ctx.fillRect(250,100,200,100);
+    
+    // =====> Animation
+
+        // window.setInterval(function(){
+        //     animate();
+        // },60);
+        this.animate();
+};
+
+function animate(){
+    update();
+    draw();
+    window.requestAnimationFrame(function(){
+        animate();
+    })
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.fillStyle = 'Navy';
+    ctx.beginPath();
+    ctx.arc(posX, posY, radius, 0, 2*Math.PI, false);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function update(){
+    // Move right until react canvas width
+    if(dir > 0){
+        dir = (posX < canvasWidth - radius) ? 1 : -1;
+    }
+
+    // Move left until we react 0
+    if(dir < 0){
+        dir = (posX > 0 + radius) ? -1 : 1;
+    }
+
+    posX += speed * dir;
 
 }
