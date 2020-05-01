62002e7d5788ae93194e6f813abef078cc874467
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-01 17:52:26 +0530

    Radial Gradient Rendering

--- Manipulating_Pixels.js
+++ Manipulating_Pixels.js
@@ -4,16 +4,27 @@ window.onload = function () {
 
     // =====> Rendering Linear Gradients
 
+        // ctx.strokeStyle = 'black';
+        
+        // let gradient = ctx.createLinearGradient(200, 200, 600, 600);
+        
+        // gradient.addColorStop(0, 'white');
+        // gradient.addColorStop(0.5, 'green');
+        // gradient.addColorStop(1, 'blue');
+        // ctx.fillStyle = gradient;
+        // ctx.rect(200,200,400,400);
+        // ctx.fill();
+
+    // =====> Rendering Radial Gradient
+
         ctx.strokeStyle = 'black';
-        
-        let gradient = ctx.createLinearGradient(200, 200, 600, 600);
-        
+
+        let gradient = ctx.createRadialGradient(250,250,300,375,375,300);
         gradient.addColorStop(0, 'white');
-        gradient.addColorStop(0.5, 'green');
-        gradient.addColorStop(1, 'blue');
+        gradient.addColorStop(0.5, 'lightgreen');
+        gradient.addColorStop(1, 'lightblue');
         ctx.fillStyle = gradient;
-        ctx.rect(200,200,400,400);
+        ctx.arc(300,300,300,0,2*this.Math.PI,false);
+        ctx.stroke();
         ctx.fill();
-        
-        
 }
