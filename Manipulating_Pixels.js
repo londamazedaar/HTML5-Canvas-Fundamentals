fbd111d99f598c6271680673b6396a456508470e
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-01 17:25:01 +0530

    Linear Gradient rendering

--- Manipulating_Pixels.js
+++ Manipulating_Pixels.js
@@ -2,5 +2,18 @@ window.onload = function () {
     const canvas = this.document.getElementById('myCanvas');
     const ctx = canvas.getContext('2d');
 
-    
+    // =====> Rendering Linear Gradients
+
+        ctx.strokeStyle = 'black';
+        
+        let gradient = ctx.createLinearGradient(200, 200, 600, 600);
+        
+        gradient.addColorStop(0, 'white');
+        gradient.addColorStop(0.5, 'green');
+        gradient.addColorStop(1, 'blue');
+        ctx.fillStyle = gradient;
+        ctx.rect(200,200,400,400);
+        ctx.fill();
+        
+        
 }
